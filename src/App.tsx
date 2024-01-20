98a3e2b4e7d76ecd348d3ce0e97a0b53a3f31335
Thushal Himaranga <thushal.himaranga@digiratina.com>
2024-01-21 00:28:50 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ComingSoonPage from './comingSoon/ComingSoon';
+
+function App() {
+  return (
+    <ComingSoonPage/>
+  );
+}
+
+export default App;
